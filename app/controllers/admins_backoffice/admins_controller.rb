8d91687e58c70d04ff38b9a640f991b30e2100c3
tomazcunha <tomazmcn@gmail.com>
2019-10-20 00:14:28 -0300

    Finalizando o formulário de edição do Admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,4 +6,15 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def edit
     @admin = Admin.find(params[:id])
   end
+
+  def update
+    admin = Admin.find(params[:id]) # variavel local se @
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+
+    if admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado!"
+    else
+      render :edit  # rederiza novamente edit caso falhe o update
+    end
+  end
 end
