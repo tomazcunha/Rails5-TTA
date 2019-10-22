3d60f4b0e948a481e5bd033dc7c7e0b6a1a25b3c
tomazcunha <tomazmcn@gmail.com>
2019-10-22 12:53:36 -0300

    Incrementando o i18n para os Models

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,19 @@
 module AdminsBackofficeHelper
+  # def translate_attribute(object, method)
+  #   object.model.human_attribute_name(method)
+  # end
+
+  # def translate_attribute(object = nil , method = nil)
+  #   if object && method
+  #     object.model.human_attribute_name(method)
+  #   else
+  #     "Informe os parâmetros corretamente!"
+  #   end
+  # end
+
+  def translate_attribute(object = nil , method = nil)
+    # usando ternário
+    (object && method) ? object.model.human_attribute_name(method) : "Informe os parâmetros corretamente!"
+  end
+
 end
