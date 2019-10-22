6193e2aa095ccd662521d81d38b3b8a2d8fb673b
tomazcunha <tomazmcn@gmail.com>
2019-10-22 01:58:37 -0300

    Criando as views (new e edit) para as Questões

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -42,7 +42,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   private
 
   def params_question
-    params.require(:question).permit(:description)
+    params.require(:question).permit(:description, :subject_id)
   end
 
   def set_question
