592081d0aa7f6e8bac045164b12968e6355c1096
tomazcunha <tomazmcn@gmail.com>
2019-10-23 18:06:06 -0300

    Conhecendo o devise_controller e o resource_class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    # helpers
+    # puts ">>>>>>>>>>>>> #{devise_controller?}"  # retorna que é o controller atual.
+    # puts ">>>>>>>>>>>>> #{resource_class}"      # quem é a classe
+    # "application"    # retorna 'application' para o layout
+
+    if devise_controller? && resource_class == Admin
+      "admin_devise"  # aplica este layout
+    else
+      "application"   # layout padrão
+    end
+  end
 end
