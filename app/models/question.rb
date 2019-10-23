34ca9afe41216584691db0b13291c36b5e3f1bc8
tomazcunha <tomazmcn@gmail.com>
2019-10-22 23:19:47 -0300

    Entendendo Nested Attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :subject
+  has_many :answers   # plural
+
+  accepts_nested_attributes_for :answers
+  # question deve aceitar atributos das respostas
 end
