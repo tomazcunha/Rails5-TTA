89fb0f27601b29c4a563d17cb64585bd85f1073a
tomazcunha <tomazmcn@gmail.com>
2019-10-24 18:52:04 -0300

    Uma pitada de metaprogramação no login Devise User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,24 @@ class ApplicationController < ActionController::Base
     # puts ">>>>>>>>>>>>> #{resource_class}"      # quem é a classe
     # "application"    # retorna 'application' para o layout
 
-    if devise_controller? && resource_class == Admin
-      "admin_devise"  # aplica este layout
-    else
-      "application"   # layout padrão
-    end
+    # if devise_controller? && resource_class == Admin
+    #   "admin_devise"  # aplica este layout
+    # elsif devise_controller? && resource_class == User
+    #   "user_devise"
+    # else
+    #   "application"   # layout padrão
+    # end
+
+    # if devise_controller?
+    #   "#{resource_class.to_s.downcase}_devise"    # "admin_devise / user_devise"
+    # else
+    #   "application"   # layout padrão
+    # end
+
+
+    # Podemos adicionar quanto models do Devise for necessário, que o layout sempre vai
+    # ser referência do automaticamente.
+    devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
+
   end
 end
