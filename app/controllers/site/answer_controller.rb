d8951146a60d35aff54f648778c0d85e0063c01d
tomazcunha <tomazmcn@gmail.com>
2019-10-25 16:55:20 -0300

    Verificando a resposta correta

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -2,6 +2,10 @@
 class Site::AnswerController < SiteController
 
   def question
-    puts ">>>>>>>>>#{params[:answer]}"
+    # puts ">>>>>>>>>#{params[:answer_id]}"
+
+    @answer = Answer.find(params[:answer_id])
+    # assim poderemos verificar se o corret = true na view question
+
   end
 end
