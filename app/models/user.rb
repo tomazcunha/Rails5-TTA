b867d5587da7f709514aab9e0946135f701de6a8
tomazcunha <tomazmcn@gmail.com>
2019-10-28 23:41:22 -0300

    Adicionando o nome do usuário no Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def full_name
+    # "#{self.first_name} #{self.last_name}"
+    # [self.first_name, self.last_name].join('---') #
+    [self.first_name, self.last_name].join(' ') # juntando os valores do array
+  end
 end
