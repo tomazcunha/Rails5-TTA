ce1d51612d61dccf10a358b1bc3952f1042ede18
tomazcunha <tomazmcn@gmail.com>
2019-10-30 00:22:19 -0300

    Conhecendo e usando validações no Rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 3 }, on: :update
+    # presence:   = preenchimento obrigatório do campo
+    # length:     = quantidade de caracters
+    # on: :update = fazer validação só quando for update
 
+
+  # Virtual Attributes
   def full_name
     # "#{self.first_name} #{self.last_name}"
     # [self.first_name, self.last_name].join('---') #
