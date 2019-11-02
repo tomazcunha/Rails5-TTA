ce32d5640db27e4793e9da3ec83f16b9d05c96cc
tomazcunha <tomazmcn@gmail.com>
2019-11-01 23:08:31 -0300

    Forçando o usuário a efetuar login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
+  before_action :check_pagination
+
   private
 
   def layout_by_resource
@@ -29,4 +31,11 @@ class ApplicationController < ActionController::Base
     devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
 
   end
+
+  def check_pagination
+    unless user_signed_in?
+      params.extract!(:page)
+    end
+  end
+  
 end
