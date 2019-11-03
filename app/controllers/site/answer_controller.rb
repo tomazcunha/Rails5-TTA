6086e90238abbdeb687a3e55fb6bcc08c36226ec
tomazcunha <tomazmcn@gmail.com>
2019-11-03 00:51:44 -0300

    Refatorando o UserStatistic

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,23 +1,12 @@
-# class Site::AnswerController < ApplicationController
 class Site::AnswerController < SiteController
 
   def question
     # puts ">>>>>>>>>#{params[:answer_id]}"
     @answer = Answer.find(params[:answer_id]) # assim poderemos verificar se o correct = true na view question
-    set_user_statistic(@answer)
+
+    # set_user_statistic(@answer)
+    UserStatistic.set_statistic(@answer, current_user)
+      # Agora o modelo vai ser responsável por essa ação
   end
 
-  private
-
-  def set_user_statistic(answer)
-    if user_signed_in?
-      user_statistic = UserStatistic.find_or_create_by(user: current_user)
-      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
-      user_statistic.save
-    end
-      # Verifica se está logado
-      # Procura se existir o usuário atual na tabela. Se não existir, cria.
-      # Se a resposta estiver correta ou não, soma um dos campos.
-      # salva
-  end
 end
--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -5,4 +5,19 @@ class UserStatistic < ApplicationRecord
   def total_questions
     self.right_questions + self.wrong_questions
   end
+
+
+  # Class Method (self)
+  def self.set_statistic(answer, current_user)
+    if !!current_user
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+      user_statistic.save
+    end
+      # Verifica se está logado
+      # Procura se existir o usuário atual na tabela. Se não existir, cria.
+      # Se a resposta estiver correta ou não, soma um dos campos.
+      # salva
+
+  end
 end
